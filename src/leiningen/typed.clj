354cda3a71cc97ea9d35d46144e6a43c55d3154e
Ben Mabey <ben@benmabey.com>
2013-09-30 18:30:18 -0600

    exit w/non-zero code if type checking fails in `check` task

--- src/leiningen/typed.clj
+++ src/leiningen/typed.clj
@@ -1,31 +1,38 @@
 (ns leiningen.typed
-  (:require [leiningen.core.eval :refer [eval-in-project]]))
+  (:require [leiningen.core.eval :refer [eval-in-project]]
+            [leiningen.core.main :as main]))
 
 (defn help []
   (println "lein-typed: Type checking for Clojure")
   (println "Usage:")
-  (println 
-" lein typed check        - type check all namespaces declared in project.clj, 
+  (println
+" lein typed check        - type check all namespaces declared in project.clj,
                            via :core.typed {:check [...]}")
   (println " lein typed check nsym+ - only type check namespaces nsyms")
-  (println 
-" lein typed coverage - basic type coverage for all namespaces declared in project.clj, 
+  (println
+" lein typed coverage - basic type coverage for all namespaces declared in project.clj,
                        via :core.typed {:check [...]}")
   (println " lein typed coverage nsym+ - basic type coverage for namespaces nsyms")
   (flush))
 
 (defn check [project & args]
-  (let [nsyms (or (when args (map symbol args)) (-> project :core.typed :check))]
-    (eval-in-project project
-                     `(if-let [nsyms# (seq '~nsyms)]
-                        (doseq [nsym# nsyms#]
-                          (try (clojure.core.typed/check-ns nsym#)
-                               (catch Exception e#
-                                 (println (.getMessage e#))
-                                 (flush))))
-                        (do (println "No namespaces provided in project.clj. Add namespaces in :core.typed {:check [...]}")
-                            (flush)))
-                     '(require '[clojure.core.typed]))))
+  (let [nsyms (or (when args (map symbol args)) (-> project :core.typed :check))
+        exit-code (eval-in-project project
+                                   `(if-let [nsyms# (seq '~nsyms)]
+                                      (let [errors# (doall
+                                                     (for [nsym# nsyms#]
+                                                       (try (clojure.core.typed/check-ns nsym#)
+                                                            (catch Exception e#
+                                                              (println (.getMessage e#))
+                                                              (flush)
+                                                              false))))]
+                                        (when-not (every? #(= % :ok) errors#)
+                                          (System/exit 1)))
+                                      (do (println "No namespaces provided in project.clj. Add namespaces in :core.typed {:check [...]}")
+                                          (flush)))
+                                   '(require '[clojure.core.typed]))]
+    (when (and (number? exit-code) (pos? exit-code))
+      (main/exit exit-code))))
 
 (defn coverage [project & args]
   (let [nsyms (or (when args (map symbol args)) (-> project :core.typed :check))]
