e865ae32ce6dd6cebd1f09d5ba1a60438a8de68e
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2014-07-20 18:26:49 +0800

    add license and url to project.clj

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
 (defproject lein-typed "0.3.5"
   :description "Type checking with Typed Clojure"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://github.com/typedclojure/lein-typed"
   :eval-in-leiningen true)
