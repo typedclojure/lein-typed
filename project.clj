a2e37c440f5a8d38c0712070235daca7957b702b
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2017-09-09 07:16:20 -0400

    release 0.4.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-typed "0.4.1-SNAPSHOT"
+(defproject lein-typed "0.4.1"
   :description "Type checking with Typed Clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
